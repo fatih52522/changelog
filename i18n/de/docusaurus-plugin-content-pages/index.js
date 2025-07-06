42ef201b58a70c00e0390d48e239ae1d44d29bd3
Fatih Simsek <fatih.simsek5209@gmail.com>
2025-07-06 14:02:36 +0200

    lets see

--- i18n/de/docusaurus-plugin-content-pages/index.js
+++ i18n/de/docusaurus-plugin-content-pages/index.js
@@ -2,10 +2,8 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
-import Translate from '@docusaurus/Translate';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
