8447b0bd8e655284107df526e73115178ce97421
Fatih Simsek <fatih.simsek5209@gmail.com>
2025-07-06 16:03:28 +0200

    f

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,6 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-    
       title={`Changelog - ${siteConfig.title}`}
       description="welcome to SCNX Changelog!">
       <HomepageHeader />
