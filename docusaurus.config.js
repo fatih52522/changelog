528e48a96500db22541957ae6454cdc11e2bfb52
Fatih Simsek <fatih.simsek5209@gmail.com>
2025-07-07 17:29:05 +0200

    changed domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://www.fatih-sim.com',
+  url: 'https://www.scnx-changelog.de',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
